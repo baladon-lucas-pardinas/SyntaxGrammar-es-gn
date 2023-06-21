33c3218848a39bfedab7bdb8de6f104c375404c3
vito3800 <victoria382000@gmail.com>
2023-06-21 18:05:37 -0300

    reglas

--- grammar/guarani/translate/leaves/determiners.py
+++ grammar/guarani/translate/leaves/determiners.py
@@ -8,7 +8,9 @@ def translate_determiners(tree,detCSV):
     possPer = label['POSSPER']
     possNum = label['POSSNUM']
     
+    ## Tener cuidado de solo mirar TER cuando se conjuga en tercera persona
+    ## Actualmente la presencia (PRES) no se utiliza
     for row in detCSV:
         if row[12] == tree['word'] and row[15].lower() == typ.lower() and row[18].lower() == num.lower() and row[17].lower() == gen.lower() and row[16] == str(possPer) and row[19] == str(possNum):
-            dets.append({'det':row[0],'persona':row[4],'numero':row[6],'possesor':row[7],'incluyente': row[8], 'nasal':row[9], 'tercero':row[10],'presencia':row[11]})
+            dets.append(row[0],{'PER':row[4],'NUM':row[6],'POSS':row[7],'INC': row[8], 'NAS':row[9], 'TER':row[10],'PRES':row[11]})
     return dets
--- grammar/guarani/translate/leaves/verbs.py
+++ grammar/guarani/translate/leaves/verbs.py
@@ -8,5 +8,5 @@ def translate_verbs(tree,verbCSV):
     num = agreement['NUM']
     for row in verbCSV:
         if row[12] == tree['word'] and row[16].lower() == mood.lower() and row[17].lower() == tense.lower() and row[18] == str(per) and row[19].lower() == num.lower():
-            verbs.append({'verbo':row[0],'inclusion':row[7],'posicion':row[8]})
+            verbs.append(row[0],{'INC':row[7],'POS':row[8]})
     return verbs
