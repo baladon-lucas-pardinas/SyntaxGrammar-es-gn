8155d685149cb89f5014aaa68c7d525f06094a4b
Agustín Lucas <agustinlucasuy@gmail.com>
2023-07-04 18:08:54 -0300

    Added NEG, PP rules

--- grammar/grammars/seventh-grammar/grammar_productions/np_subject.py
+++ grammar/grammars/seventh-grammar/grammar_productions/np_subject.py
@@ -10,15 +10,20 @@ S[AGR=?a] -> NP[AGR=?a] VP[AGR=?a, MOOD=i] PP
 VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m, SUBCAT='intr']
 VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m, SUBCAT='tr'] NP
 VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m, SUBCAT='di'] NP1 AA NP2
+VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m, SUBCAT='intr']
+VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m, SUBCAT='tr'] NP
+VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m, SUBCAT='di'] NP1 AA NP2
+# PP expansion productions
+PP -> PR NP
 # NP1 expansion productions
 NP1[AGR=?a] -> NP[AGR=?a] 
 # NP2 expansion productions
 NP2[AGR=?a] -> NP[AGR=?a]
-# AA expansion productions
-AA -> 'a'
 # NP expansion productions
 NP[AGR=?a] -> D[AGR=?a] N[AGR=?a]
 # ###################
 # Lexical Productions
 # ###################
+AA -> 'a'
+NEG -> 'no'
 """
