2101518bbd6a5965530c0d31a63db1eba34bade3
vito3800 <victoria382000@gmail.com>
2023-08-09 20:25:27 -0300

    adpositions

--- grammar/guarani/translate/leaves/adpositions.py
+++ grammar/guarani/translate/leaves/adpositions.py
@@ -3,10 +3,11 @@ def translate_adpositions(tree,adpCSV):
     for row in adpCSV:
         if row[6] == tree['word']:
             if (row[5] == '0'):
-                adpRes.append((row[0],{'AGR':{}, 'S':row[4]}))
+                adpRes.append((row[0],{'AGR':{}, 'U':row[3], 'S':row[4],'NF':'N'}))
+                adpRes.append((row[0],{'AGR':{}, 'U':row[3], 'S':row[4],'NF':'O'}))
             else:
                 if (row[3] == 'U'):
                     adpRes.append((row[0],{'AGR':{}, 'U':row[3], 'S':row[4],'NF':row[5]}))
                 else:
-                    adpRes.append((row[0],{'AGR':{'NAS':row[5]}, 'U':row[3], 'S':row[4]}))
+                    adpRes.append((row[0],{'AGR':{'NAS':row[5]}, 'U':row[3], 'S':row[4],'NF':'0'}))
     return adpRes
