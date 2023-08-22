684f5903379ede4cf3a5cc1c5e15d78a9e18f06e
Agustin Lucas <agustin.lucas@fing.edu.uy>
2023-08-22 19:39:00 -0300

    Script skeleton

--- grammar/parsing-subtrees/parse-subtrees-2.py
+++ grammar/parsing-subtrees/parse-subtrees-2.py
@@ -1,54 +1,85 @@
+import argparse
+import csv
 from nltk import grammar, FeatureChartParser
+
 ### WHAT I SHOULD DO HERE INSTEAD IS SPLIT BY UNKNOWN WORDS AND THEN PARSE EACH SUBSTRING
 ### I SHOULD READ FROM MY EXTRACTED.CSV
 ### FOR PARSING EACH SUBSTRING I SHOULD DO A SORT OF MAXMATCH, BUT THAT CAN BE ANOTHER SCRIPT, V3
 
-# Define your grammar rules here
-grammar_rules = """
-    S -> NP VP
-    NP -> Det N
-    VP -> V NP | V
-    Det -> 'the' | 'a'
-    N -> 'dog' | 'cat'
-    V -> 'chased' | 'slept'
-"""
-my_grammar = grammar.FeatureGrammar.fromstring(grammar_rules)
+# Function to read a text file into a string
+def read_text_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
 
-# Create a FeatureChartParser instance
-parser = FeatureChartParser(my_grammar)
+# Function to read sentences from a CSV file into a list
+def read_sentences_from_csv(file_path):
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        return [row[0] for row in csv_reader] # Assuming single-column CSV
 
-def partial_parse(sentence):
-    words = sentence.split()
-    partial_trees = []
-    s_parse_found = False
-    start_index = 0
+# Function to split sentences
+def split_sentence(sentence: str, feature_parser: FeatureChartParser) -> list[str]:
+    words = sentence.split()  # Split the sentence into words
+    sub_sentences = []
+    current_sub_sentence = []
 
-    for i in range(len(words)):
-        if words[i] not in my_grammar._lexical_index:
-            if i > start_index:
-                substring = " ".join(words[start_index:i])
-                parses = list(parser.parse(substring.split()))
-                if parses:
-                    if parses[-1].label() == 'S':
-                        s_parse_found = True
-                    partial_trees.append((substring, parses[-1]))
-            start_index = i + 1
+    for word in words:
+        if word in feature_parser.grammar().lexicon():  # Check if the word is known in the grammar
+            current_sub_sentence.append(word)
+        else:
+            if current_sub_sentence:  # Append current sub_sentence if not empty
+                sub_sentences.append(" ".join(current_sub_sentence))
+            current_sub_sentence = []
 
-    # Parse the last segment if it's not fully covered
-    if start_index < len(words):
-        substring = " ".join(words[start_index:])
-        parses = list(parser.parse(substring.split()))
-        if parses:
-            if parses[-1].label() == 'S':
-                s_parse_found = True
-            partial_trees.append((substring, parses[-1]))
+    if current_sub_sentence:  # Append the last sub_sentence if not empty
+        sub_sentences.append(" ".join(current_sub_sentence))
 
-    return partial_trees
+    return sub_sentences
 
-sentence = "the dog chased the cat and the cat slept and the cat chased and the dog slept"
-partial_results = partial_parse(sentence)
+# Function to perform the max_match operation (placeholder, implement later)
+def max_match(sentence, feature_parser):
+    # Implement your max_match logic here
+    pass
 
-for substring, parse_tree in partial_results:
-    print(f"Partial parse for '{substring}':")
-    print(parse_tree)
-    print()
+# Function to write trees to a txt file
+def write_trees_to_file(trees, output_file):
+    with open(output_file, 'w') as file:
+        for tree in trees:
+            file.write(str(tree) + '\n')
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Process sentences using a FeatureChartParser')
+    parser.add_argument('--grammar', required=True, help='Path to the feature grammar text file')
+    parser.add_argument('--input', required=True, help='Path to the input CSV file containing sentences')
+    parser.add_argument('--output', required=True, help='Path to the output txt file for trees')
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+
+    # Read the feature grammar from a file and create a feature grammar
+    grammar_string = read_text_file(args.grammar)
+    my_grammar = grammar.FeatureGrammar.fromstring(grammar_string)
+
+    # Create a FeatureChartParser based on the grammar
+    feature_parser = FeatureChartParser(my_grammar)
+
+    # Read sentences from the CSV file
+    sentences = read_sentences_from_csv(args.input)
+
+    # Initialize a list to store trees
+    trees = []
+
+    # Loop through sentences, split them, and perform max_match
+    for sentence in sentences:
+        sub_sentences = split_sentence(sentence)
+        for sub_sentence in sub_sentences:
+            tree = max_match(sub_sentence, feature_parser)
+            if tree is not None:
+                trees.append(tree)
+
+    # Write trees to the output txt file
+    write_trees_to_file(trees, args.output)
+
+if __name__ == '__main__':
+    main()
