d47396be064944712af70f024c681753806c1b74
Agustin Lucas <agustin.lucas@fing.edu.uy>
2023-09-07 21:39:11 -0300

    Removed transitivities from ancora

--- grammar/grammars/ancora-grammar/grammar_productions/ancora_grammar.py
+++ grammar/grammars/ancora-grammar/grammar_productions/ancora_grammar.py
@@ -14,12 +14,12 @@ S[AGR=?a] -> VP[AGR=?a, MOOD=i] PP
 S[AGR=?a] -> NP[AGR=?a]
 S -> PP
 # VP expansion productions
-VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m, SUBCAT='intr']
-VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m, SUBCAT='tr'] NP
-VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m, SUBCAT='di'] NP PPA
-VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m, SUBCAT='intr']
-VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m, SUBCAT='tr'] NP
-VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m, SUBCAT='di'] NP PPA
+VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m]
+VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m] NP
+VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m] NP PPA
+VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m]
+VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m] NP
+VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m] NP PPA
 # PP expansion productions
 PP -> PR NP
 # PPA expansion productions
--- grammar/grammars/ancora-grammar/lexical_productions/verbs.py
+++ grammar/grammars/ancora-grammar/lexical_productions/verbs.py
@@ -10,13 +10,7 @@ def write_verb_productions(grammar_file, verb_lexicon, transitivities):
             # This is to use the appropriate columns in all.verbs.csv
             shifted_row = ["dummy"] + row
             word, infinitive, mood, tense, person, number = shifted_row[1], shifted_row[2], shifted_row[5], shifted_row[6], shifted_row[7], shifted_row[8]
-            # Let's omit for now the non-indicative moods, and assign all transitivities to unknown verbs
+            # Let's omit for now the non-indicative moods, and disregard transitivities
             if mood == 'I':
-                transitivity = transitivities.get(infinitive)
-                if not transitivity or transitivity['transitive']: 
-                    grammar_file.write(f"V[AGR=[NUM={number.lower()}, PER={person}, TENSE={tense.lower()}], MOOD={mood.lower()}, SUBCAT='tr'] -> '{word}'\n")
-                if not transitivity or transitivity['intransitive']:
-                    grammar_file.write(f"V[AGR=[NUM={number.lower()}, PER={person}, TENSE={tense.lower()}], MOOD={mood.lower()}, SUBCAT='intr'] -> '{word}'\n")
-                if not transitivity or transitivity['ditransitive']:
-                    grammar_file.write(f"V[AGR=[NUM={number.lower()}, PER={person}, TENSE={tense.lower()}], MOOD={mood.lower()}, SUBCAT='di'] -> '{word}'\n")
+                grammar_file.write(f"V[AGR=[NUM={number.lower()}, PER={person}, TENSE={tense.lower()}], MOOD={mood.lower()}] -> '{word}'\n")
             #grammar_file.write(f"V[AGR=[NUM={number.lower()}, PER={person}, TENSE={tense.lower()}]] -> '{word}'\n")
