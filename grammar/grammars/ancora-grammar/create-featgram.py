85ac0c463a89e1ebdff2597aa7beac500c7f820a
Agustin Lucas <agustin.lucas@fing.edu.uy>
2023-09-07 21:58:47 -0300

    Added connectors to Ancora

--- grammar/grammars/ancora-grammar/create-featgram.py
+++ grammar/grammars/ancora-grammar/create-featgram.py
@@ -7,6 +7,7 @@ from .lexical_productions.determiners import write_determiner_productions
 from .lexical_productions.pronouns import write_pronoun_productions
 from .lexical_productions.adpositions import write_adposition_productions
 from .lexical_productions.adjectives import write_adjective_productions
+from .lexical_productions.connectors import write_connector_productions
 from .utils.read_config import read_config
 from .utils.read_transitivities import read_transitivities
 from .grammar_productions.ancora_grammar import grammar as ancora_grammar_string
@@ -37,6 +38,8 @@ def main():
             write_adposition_productions(grammar_file, config['adpositions'])
         if 'adjectives' in config:
             write_adjective_productions(grammar_file, config['adjectives'])
+        if 'connectors' in config:
+            write_connector_productions(grammar_file, config['connectors'])
 
     remove_duplicates(output_file)
 
--- grammar/grammars/ancora-grammar/grammar_productions/ancora_grammar.py
+++ grammar/grammars/ancora-grammar/grammar_productions/ancora_grammar.py
@@ -9,10 +9,12 @@ S[AGR=?a] -> NP[AGR=?a] VP[AGR=?a, MOOD=i] PP
 S[AGR=?a] -> P[AGR=?a, CASE=n] VP[AGR=?a, MOOD=i]
 S[AGR=?a] -> P[AGR=?a, CASE=n] VP[AGR=?a, MOOD=i] PP
 # For extra Ancora matching
-S[AGR=?a] -> VP[AGR=?a, MOOD=i]
-S[AGR=?a] -> VP[AGR=?a, MOOD=i] PP
-S[AGR=?a] -> NP[AGR=?a]
+S -> VP
+S -> VP PP
+S -> NP
+S -> N
 S -> PP
+S -> CON
 # VP expansion productions
 VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m]
 VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m] NP
