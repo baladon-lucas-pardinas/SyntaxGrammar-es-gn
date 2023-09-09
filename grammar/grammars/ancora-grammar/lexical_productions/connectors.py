b069b18d3677005e71888bfd9cfa2bc0b10f85da
Agustin Lucas <agustin.lucas@fing.edu.uy>
2023-09-09 18:55:17 -0300

    Fixed function name

--- grammar/grammars/ancora-grammar/lexical_productions/connectors.py
+++ grammar/grammars/ancora-grammar/lexical_productions/connectors.py
@@ -1,7 +1,7 @@
 ### Read: https://www.nltk.org/book/ch09.html
 import csv
 
-def write_connectors_productions(grammar_file, connectors_lexicon):
+def write_connector_productions(grammar_file, connectors_lexicon):
     # Load connectors lexical rules from CSV
     with open(connectors_lexicon, newline='', encoding='utf-8') as csvfile:
         reader = csv.reader(csvfile)
