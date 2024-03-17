8a55246b9637f3c0cad4c19c22c66b857aba74e3
Agustin Lucas <agustin.lucas@fing.edu.uy>
2024-03-17 16:29:28 -0300

    Added the option to generate sentences without having to limit subject type

---
+++ grammar/grammars/ninth-grammar/grammar_productions/all_subject.py
@@ -0,0 +1,31 @@
+### Read: https://www.nltk.org/book/ch09.html
+grammar = """% start S
+# ###################
+# Grammar Productions
+# ###################
+# S expansion productions
+S[AGR=?a] -> NP[AGR=?a] VP[AGR=?a, MOOD=i]
+S[AGR=?a] -> NP[AGR=?a] VP[AGR=?a, MOOD=i] PP
+S[AGR=?a] -> P[AGR=?a, CASE=n] VP[AGR=?a, MOOD=i]
+S[AGR=?a] -> P[AGR=?a, CASE=n] VP[AGR=?a, MOOD=i] PP
+# VP expansion productions
+VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m, SUBCAT=intr]
+VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m, SUBCAT=tr] NP
+VP[AGR=?a, MOOD=?m] -> V[AGR=?a, MOOD=?m, SUBCAT=di] NP PPA
+VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m, SUBCAT=intr]
+VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m, SUBCAT=tr] NP
+VP[AGR=?a, MOOD=?m] -> NEG V[AGR=?a, MOOD=?m, SUBCAT=di] NP PPA
+# PP expansion productions
+PP -> PR NP
+# PPA expansion productions
+PPA[AGR=?a] -> AA NP[AGR=?a]
+# NP expansion productions
+NP[AGR=?a] -> D[AGR=?a] N[AGR=?a]
+NP[AGR=?a] -> D[AGR=?a] N[AGR=?a] A[AGR=?a]
+NP[AGR=?a] -> D[AGR=?a] A[AGR=?a] N[AGR=?a]
+# ###################
+# Lexical Productions
+# ###################
+AA -> 'a'
+NEG -> 'no'
+"""
